a24f5e51ed9c2d9a7bbedd06c19d3593effa56e6
Franz Zemen <franzzemen@gmail.com>
2019-12-30 14:10:55 -0500

    Trial function using ES6 modules

---
+++ Gulpbase.js
@@ -0,0 +1,11 @@
+// Define directories used in functions here which means they will execute from <project>/node_modules/@franzzemen/gulp-base
+const releaseDir = '../../../release';
+const buildDir = '../../../build';
+
+function cleanRelease() {
+  return del(releaseDir);
+}
+
+export {
+  cleanRelease
+}
